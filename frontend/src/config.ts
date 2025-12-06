b8ae6be4aa5688bf32bcc060086ad93f99f865b1
michalpazur <michal.pazur1@gmail.com>
2025-12-06 15:32:48 +0100

    Add: missing candidates avatars

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,11 +1,5 @@
 import chroma from "chroma-js";
-import biejat from "./static/biejat.png";
-import braun from "./static/braun.png";
-import holownia from "./static/holownia.png";
-import mentzen from "./static/mentzen.png";
-import nawrocki from "./static/nawrocki.png";
-import trzaskowski from "./static/trzaskowski.png";
-import zandberg from "./static/zandberg.png";
+import { presidentialCandidates } from "./static";
 
 export type CandidateId =
   | "bartosiewicz"
@@ -34,39 +28,51 @@ type Candidate = {
 export const GRADIENT_COLORS = 5;
 
 export const candidatesConfig: Record<CandidateId, Candidate> = {
-  bartosiewicz: { name: "Artur Bartosiewicz", id: "bartosiewicz" },
+  bartosiewicz: {
+    name: "Artur Bartoszewicz",
+    id: "bartosiewicz",
+    avatarUrl: presidentialCandidates.bartoszewicz,
+  },
   biejat: {
     name: "Magdalena Biejat",
     id: "biejat",
     color: "#8ACE00",
     gradient: chroma.scale(["#B4FF1C", "#689B00"]).colors(GRADIENT_COLORS),
-    avatarUrl: biejat,
-    maxGradient: 20,
+    avatarUrl: presidentialCandidates.biejat,
+    maxGradient: 15,
   },
   braun: {
     name: "Grzegorz Braun",
     id: "braun",
     color: "#5D4037",
     gradient: chroma.scale(["#A1887F", "#3E2723"]).colors(GRADIENT_COLORS),
-    avatarUrl: braun,
-    maxGradient: 15,
+    avatarUrl: presidentialCandidates.braun,
+    maxGradient: 20,
   },
   holownia: {
     name: "Szymon Hołownia",
     id: "holownia",
     color: "#FBC02D",
     gradient: chroma.scale(["#FFF176", "#F57F17"]).colors(GRADIENT_COLORS),
-    avatarUrl: holownia,
-    maxGradient: 15,
+    avatarUrl: presidentialCandidates.holownia,
+    maxGradient: 20,
   },
-  jakubiak: { name: "Marek Jakubiak", id: "jakubiak" },
-  maciak: { name: "Maciej Maciak", id: "maciak" },
+  jakubiak: {
+    name: "Marek Jakubiak",
+    id: "jakubiak",
+    avatarUrl: presidentialCandidates.jakubiak,
+  },
+  maciak: {
+    name: "Maciej Maciak",
+    id: "maciak",
+    avatarUrl: presidentialCandidates.maciak,
+  },
   mentzen: {
     name: "Sławomir Mentzen",
     id: "mentzen",
     color: "#303F9F",
     gradient: chroma.scale(["#7986CB", "#1A237E"]).colors(GRADIENT_COLORS),
-    avatarUrl: mentzen,
+    avatarUrl: presidentialCandidates.mentzen,
     maxGradient: 40,
   },
   nawrocki: {
@@ -74,23 +80,35 @@ export const candidatesConfig: Record<CandidateId, Candidate> = {
     id: "nawrocki",
     color: "#1976D2",
     gradient: chroma.scale(["#64B5F6", "#0D47A1"]).colors(GRADIENT_COLORS),
-    avatarUrl: nawrocki,
+    avatarUrl: presidentialCandidates.nawrocki,
   },
-  senyszyn: { name: "Joanna Senyszyn", id: "senyszyn" },
-  stanowski: { name: "Krzysztof Stanowski", id: "stanowski" },
+  senyszyn: {
+    name: "Joanna Senyszyn",
+    id: "senyszyn",
+    avatarUrl: presidentialCandidates.senyszyn,
+  },
+  stanowski: {
+    name: "Krzysztof Stanowski",
+    id: "stanowski",
+    avatarUrl: presidentialCandidates.stanowski,
+  },
   trzaskowski: {
     name: "Rafał Trzaskowski",
     id: "trzaskowski",
     color: "#E64A19",
     gradient: chroma.scale(["#FF8A65", "#BF360C"]).colors(GRADIENT_COLORS),
-    avatarUrl: trzaskowski,
+    avatarUrl: presidentialCandidates.trzaskowski,
   },
-  woch: { name: "Marek Woch", id: "woch" },
+  woch: {
+    name: "Marek Woch",
+    id: "woch",
+    avatarUrl: presidentialCandidates.woch,
+  },
   zandberg: {
     name: "Adrian Zandberg",
     id: "zandberg",
     color: "#7B1FA2",
-    avatarUrl: zandberg,
+    avatarUrl: presidentialCandidates.zandberg,
     gradient: chroma.scale(["#BA68C8", "#4A148C"]).colors(GRADIENT_COLORS),
     maxGradient: 20,
   },
---
+++ frontend/src/static/index.ts
@@ -0,0 +1,29 @@
+import bartoszewicz from "./bartoszewicz.png";
+import biejat from "./biejat.png";
+import braun from "./braun.png";
+import holownia from "./holownia.png";
+import jakubiak from "./jakubiak.png";
+import maciak from "./maciak.png";
+import mentzen from "./mentzen.png";
+import nawrocki from "./nawrocki.png";
+import senyszyn from "./senyszyn.png";
+import stanowski from "./stanowski.png";
+import trzaskowski from "./trzaskowski.png";
+import woch from "./woch.png";
+import zandberg from "./zandberg.png";
+
+export const presidentialCandidates = {
+  bartoszewicz,
+  biejat,
+  braun,
+  holownia,
+  jakubiak,
+  maciak,
+  mentzen,
+  nawrocki,
+  senyszyn,
+  stanowski,
+  trzaskowski,
+  woch,
+  zandberg,
+};
